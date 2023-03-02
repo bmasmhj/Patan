13f3c42a68bf1a3a7257f467986c45fb71a35c76
bmasmhj <bmasmhj@gmail.com>
2023-03-02 15:36:03 +0545

    header / footer

---
+++ modal/footer.php
@@ -0,0 +1,59 @@
+<!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+<?php require 'modal/contact-us.php'; ?>
+
+<footer class="footer py-10">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 mb-4 mx-auto text-center">
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+          Company
+        </a>
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+          About Us
+        </a>
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+          Team
+        </a>
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+          Products
+        </a>
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+          Blog
+        </a>
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+          Pricing
+        </a>
+      </div>
+      <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+          <span class="text-lg fab fa-dribbble"></span>
+        </a>
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+          <span class="text-lg fab fa-twitter"></span>
+        </a>
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+          <span class="text-lg fab fa-instagram"></span>
+        </a>
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+          <span class="text-lg fab fa-pinterest"></span>
+        </a>
+        <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+          <span class="text-lg fab fa-github"></span>
+        </a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-8 mx-auto text-center mt-1">
+        <p class="mb-0 text-secondary">
+          Copyright © <script>document.write(new Date().getFullYear())</script> Soft by Creative Tim.
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+<script src="assets/js/main.js"></script>
+<script src="assets/js/script.js"></script>
+
+</body>
+</html>
---
+++ modal/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.0/css/all.css">
+
+    <title>Patan Bca Guys</title>
+</head>
+<body>
+    
+
