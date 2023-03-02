b03f39ba8d49354398ced23a7482eef297260757
bmasmhj <bmasmhj@gmail.com>
2023-03-02 15:36:59 +0545

    index

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require 'modal/header.php'; ?>
+<?php require 'modal/nav.php'; ?>
+<?php require 'modal/hero-section.php'; ?>
+<?php require 'modal/blog.php'; ?>
+<?php require 'modal/course.php'; ?>
+
+
+
+
+
+<?php require 'modal/footer.php'; ?>
