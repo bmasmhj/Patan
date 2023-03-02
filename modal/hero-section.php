6077a36cfc59ca565b7c824f1659f658a167c781
bmasmhj <bmasmhj@gmail.com>
2023-03-02 15:35:42 +0545

    hero page

---
+++ modal/hero-section.php
@@ -0,0 +1,18 @@
+<header class="position-relative">
+  <div class="page-header min-vh-100" style="background-image: url(http://www.tribhuvan-university.edu.np/upload_file/images/slider/1670749582_20992219_fdfdfdfdf.png);">
+    <span class="mask bg-gradient-info"></span>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 text-start">
+          <h1 class="text-white">Education IS Our Third Eyes</h1>
+          <p class="lead text-white text-start pe-5 mt-4">Come Join US </p>
+          <br />
+          <div class="buttons">
+            <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModalSignup" class="btn btn-lg btn-white">Contact Us</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+</header>
