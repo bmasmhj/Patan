701198aca9d77f2a3bbce26159e0cecb8434af39
bmasmhj <bmasmhj@gmail.com>
2023-03-02 20:49:47 +0545

    link and hover

--- modal/all_course.php
+++ modal/all_course.php
@@ -6,84 +6,84 @@
       </div>
     </div>
     <div class="row mt-5">
-      <div class="col-lg-4 col-md-6 mb-5">
-        <div class="card">
+      <a href='javascript:void()' class="col-lg-4 courses col-md-6 icon-move-right mb-5"  data-aos="fade-up" data-aos-duration='100' >
+        <div class="card course" >
           <div class="card-body border-radius-lg position-relative overflow-hidden pb-4 px-5">
             <h5 class="mt-2">BCA</h5>
-<p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
-            <a href="javascript:;" class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info icon-move-right">
+            <p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
+            <div class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info ">
                 See more
             <i class="fas fa-arrow-right text-xs ms-1"></i>
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
 
-      <div class="col-lg-4 col-md-6 mb-5">
-        <div class="card">
+      <a href='javascript:void()' class="col-lg-4 courses col-md-6 icon-move-right mb-5"  data-aos="fade-up" data-aos-duration='200' >
+        <div class="card course" >
           <div class="card-body border-radius-lg position-relative overflow-hidden pb-4 px-5">
             <h5 class="mt-2">BBA</h5>
-<p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
-            <a href="javascript:;" class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info icon-move-right">
+            <p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
+            <div class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info">
                 See more
             <i class="fas fa-arrow-right text-xs ms-1"></i>
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
 
-      <div class="col-lg-4 col-md-6 mb-5">
-        <div class="card">
+      <a href='javascript:void()' class="col-lg-4 courses col-md-6 icon-move-right mb-5"  data-aos="fade-up" data-aos-duration='300' >
+        <div class="card course" >
           <div class="card-body border-radius-lg position-relative overflow-hidden pb-4 px-5">
             <h5 class="mt-2">BBS</h5>
-<p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
-            <a href="javascript:;" class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info icon-move-right">
+            <p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
+            <div class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info">
                 See more
             <i class="fas fa-arrow-right text-xs ms-1"></i>
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
 
-      <div class="col-lg-4 col-md-6 mb-5">
-        <div class="card">
+      <a href='javascript:void()' class="col-lg-4 courses col-md-6 icon-move-right mb-5"  data-aos="fade-up" data-aos-duration='400' >
+        <div class="card course" >
           <div class="card-body border-radius-lg position-relative overflow-hidden pb-4 px-5">
             <h5 class="mt-2">BA</h5>
             <p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
-            <a href="javascript:;" class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info icon-move-
+            <div class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info icon-move-
             right">
                 See more
             <i class="fas fa-arrow-right text-xs ms-1"></i>
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
 
-      <div class="col-lg-4 col-md-6 mb-5">
-        <div class="card">
+      <a href='javascript:void()' class="col-lg-4 courses col-md-6 icon-move-right mb-5"  data-aos="fade-up" data-aos-duration='500' >
+        <div class="card course" >
           <div class="card-body border-radius-lg position-relative overflow-hidden pb-4 px-5">
             <h5 class="mt-2">CSIT</h5>
             <p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
-            <a href="javascript:;" class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info icon-move-right">
+            <div class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info">
                 See more
             <i class="fas fa-arrow-right text-xs ms-1"></i>
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
 
-      <div class="col-lg-4 col-md-6 mb-5">
-        <div class="card">
+      <a href='javascript:void()' class="col-lg-4 courses col-md-6 icon-move-right mb-5"  data-aos="fade-up" data-aos-duration='600' >
+        <div class="card course" >
           <div class="card-body border-radius-lg position-relative overflow-hidden pb-4 px-5">
             <h5 class="mt-2">BIT</h5>
                 <p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
-            <a href="javascript:;" class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info icon-move-right">
+            <div class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info">
                 See more
             <i class="fas fa-arrow-right text-xs ms-1"></i>
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
 
     </div>
   </div>
--- modal/course.php
+++ modal/course.php
@@ -6,44 +6,44 @@
       </div>
     </div>
     <div class="row mt-5">
-      <div class="col-lg-4 col-md-6 mb-5">
-        <div class="card">
+      <a href='javascript:void()' class="col-lg-4 col-md-6 mb-5 courses icon-move-right"  data-aos="fade-up" data-aos-delay='300'>
+        <div class="card course">
           <div class="card-body border-radius-lg position-relative overflow-hidden pb-4 px-5">
             <h5 class="mt-2">BCA</h5>
-<p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
-            <a href="javascript:;" class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info icon-move-right">
+            <p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
+            <div class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info ">
                 See more
             <i class="fas fa-arrow-right text-xs ms-1"></i>
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
 
-      <div class="col-lg-4 col-md-6 mb-5">
-        <div class="card">
+      <a href='javascript:void()' class="col-lg-4 col-md-6 mb-5 courses icon-move-right"  data-aos="fade-up" data-aos-delay='400'>
+        <div class="card course">
           <div class="card-body border-radius-lg position-relative overflow-hidden pb-4 px-5">
             <h5 class="mt-2">BBA</h5>
-<p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
-            <a href="javascript:;" class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info icon-move-right">
+            <p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
+            <div class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info ">
                 See more
             <i class="fas fa-arrow-right text-xs ms-1"></i>
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
 
-      <div class="col-lg-4 col-md-6 mb-5">
-        <div class="card">
+      <a href='javascript:void()' class="col-lg-4 col-md-6 mb-5 courses icon-move-right"  data-aos="fade-up" data-aos-delay='500'>
+        <div class="card course">
           <div class="card-body border-radius-lg position-relative overflow-hidden pb-4 px-5">
             <h5 class="mt-2">BBS</h5>
-<p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
-            <a href="javascript:;" class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info icon-move-right">
+            <p class='text-justify mb-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
+            <div class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info ">
                 See more
             <i class="fas fa-arrow-right text-xs ms-1"></i>
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
 
      <a href="courses.php" class="text-center text-info" >See more</a>
     </div>
