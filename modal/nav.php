d513507f95e35eb8394771a02e24e960b60ff22a
bmasmhj <bmasmhj@gmail.com>
2023-03-02 15:35:34 +0545

    nav bar

---
+++ modal/nav.php
@@ -0,0 +1,52 @@
+<div class="position-fixed w-100 bg-white z-index-3" >
+    <div style='min-height:100px'>
+    <h1 class="text-center ">Patan Multiple Campus</h1>
+</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-white z-index-3 py-3">
+  <div class="container">
+    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon mt-2">
+        <span class="navbar-toggler-bar bar1"></span>
+        <span class="navbar-toggler-bar bar2"></span>
+        <span class="navbar-toggler-bar bar3"></span>
+      </span>
+    </button>
+    <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
+      <ul class="navbar-nav navbar-nav-hover mx-auto">
+        <li class="nav-item  mx-5">
+          <a href="index.php" class="nav-link nav_item p-0 d-flex justify-content-between cursor-pointer align-items-center" >
+            Home
+          </a>
+        </li>
+
+        <li class="nav-item  mx-5">
+          <a href="Courses.php" class="nav-link nav_item p-0 d-flex justify-content-between cursor-pointer align-items-center" >
+            Courses
+          </a>
+        </li>
+        <li class="nav-item  mx-5">
+          <a href="blog.php" class="nav-link nav_item p-0 d-flex justify-content-between cursor-pointer align-items-center" >
+            Blog
+          </a>
+        </li>
+
+        <li class="nav-item  mx-5">
+          <a href="About.php" class="nav-link nav_item p-0 d-flex justify-content-between cursor-pointer align-items-center" >
+            About
+          </a>
+        </li>
+
+        <li class="nav-item  mx-5">
+          <a data-bs-toggle="modal" data-bs-target="#exampleModalSignup" class="nav-link nav_item p-0 d-flex justify-content-between cursor-pointer align-items-center" >
+            Contact US
+          </a>
+        </li>
+
+      </ul>
+
+
+    
+    </div>
+  </div>
+</nav>
+</div>
