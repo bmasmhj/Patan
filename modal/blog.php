99c3dfab6a79ac56ccee032b29232f78c9721993
bmasmhj <bmasmhj@gmail.com>
2023-03-02 15:36:51 +0545

    blog modal

---
+++ modal/blog.php
@@ -0,0 +1,135 @@
+
+<section class="pt-5 pb-0">
+    <h1 class="text-center">Latest News</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-6">
+        <div class="card card-blog card-plain">
+          <div class="position-relative">
+            <a class="d-block blur-shadow-image">
+              <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/house.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
+            </a>
+          </div>
+          <div class="card-body px-1 pt-3">
+            <p class="text-gradient text-dark mb-2 text-sm">Entire Apartment • 3 Guests • 2 Beds</p>
+            <a href="javascript:;">
+              <h5>
+                Lovely and cosy apartment
+              </h5>
+            </a>
+            <p>
+              Siri&#39;s latest trick is offering a hands-free TV viewing experience, that will allow consumers to turn on or off their television, change inputs, fast forward.
+            </p>
+            <button type="button" class="btn btn-outline-primary btn-sm">From / Night</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card card-blog card-plain">
+          <div class="position-relative">
+            <a class="d-block blur-shadow-image">
+              <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/pool.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
+            </a>
+          </div>
+          <div class="card-body px-1 pt-3">
+            <p class="text-gradient text-dark mb-2 text-sm">Private Room • 1 Guests • 1 Sofa</p>
+            <a href="javascript:;">
+              <h5>
+                Single room in the center of the city
+              </h5>
+            </a>
+            <p>
+              As Uber works through a huge amount of internal management turmoil, the company is also consolidating and rationalizing more of its international business.
+            </p>
+            <button type="button" class="btn btn-outline-primary btn-sm">From / Night</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card card-blog card-plain">
+          <div class="position-relative">
+            <a class="d-block blur-shadow-image">
+              <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/antalya.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
+            </a>
+          </div>
+          <div class="card-body px-1 pt-3">
+            <p class="text-gradient text-dark mb-2 text-sm">Entire Apartment • 4 Guests • 2 Beds</p>
+            <a href="javascript:;">
+              <h5>
+                Independent house bedroom kitchen
+              </h5>
+            </a>
+            <p>
+              Music is something that every person has his or her own specific opinion about. Different people have different taste, and various types of music.
+            </p>
+            <button type="button" class="btn btn-outline-primary btn-sm">From / Night</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card card-blog card-plain">
+          <div class="position-relative">
+            <a class="d-block blur-shadow-image">
+              <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/tiny-house.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
+            </a>
+          </div>
+          <div class="card-body px-1 pt-3">
+            <p class="text-gradient text-dark mb-2 text-sm">Entire Apartment • 2 Guests • 1 Bed</p>
+            <a href="javascript:;">
+              <h5>
+                Zen Gateway with pool and garden
+              </h5>
+            </a>
+            <p>
+              Fast forward, rewind and more, without having to first invoke a specific skill, or even
+              press a button on their remote.
+            </p>
+            <button type="button" class="btn btn-outline-primary btn-sm">From / Night</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card card-blog card-plain">
+          <div class="position-relative">
+            <a class="d-block blur-shadow-image">
+              <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/air-bnb.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
+            </a>
+          </div>
+          <div class="card-body px-1 pt-3">
+            <p class="text-gradient text-dark mb-2 text-sm">Entire Flat • 8 Guests • 3 Rooms</p>
+            <a href="javascript:;">
+              <h5>
+                Cheapest hotels for a luxury vacation
+              </h5>
+            </a>
+            <p>
+              Today, the company announced it will be combining its rides-on-demand business and UberEATS.
+            </p>
+            <button type="button" class="btn btn-outline-primary btn-sm">From / Night</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card card-blog card-plain">
+          <div class="position-relative">
+            <a class="d-block blur-shadow-image">
+              <img src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/palm-house.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
+            </a>
+          </div>
+          <div class="card-body px-1 pt-3">
+            <p class="text-gradient text-dark mb-2 text-sm">Entire Apartment • 2 Guests • 1 Bed</p>
+            <a href="javascript:;">
+              <h5>
+                Cozy Double Room Near Station
+              </h5>
+            </a>
+            <p>
+              Different people have different taste, and various types of music have many ways of leaving an impact on someone.
+            </p>
+            <button type="button" class="btn btn-outline-primary btn-sm">From / Night</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
