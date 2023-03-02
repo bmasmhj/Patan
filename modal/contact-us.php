5837b7f4c38febaded51b87728c665482b37d8ed
bmasmhj <bmasmhj@gmail.com>
2023-03-02 15:36:41 +0545

    contact us popup

---
+++ modal/contact-us.php
@@ -0,0 +1,34 @@
+
+      <div class="modal fade" id="exampleModalSignup" tabindex="-1" aria-labelledby="exampleModalSignup" aria-hidden="true">
+        <div class="modal-dialog modal-danger modal-dialog-centered modal-" role="document">
+          <div class="modal-content">
+            <div class="modal-body p-0">
+              <div class="card card-plain">
+                <div class="card-header pb-0 text-start">
+                    <h3 class="font-weight-bolder text-info text-gradient">Contact US</h3>
+                    <p class="mb-0"></p>
+                </div>
+                <div class="card-body pb-3">
+                  <form role="form text-start">
+                    <label>Name</label>
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="name-addon" >
+                    </div>
+                    <label>Email</label>
+                    <div class="input-group mb-3">
+                      <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon" >
+                    </div>
+                    <label>Message</label>
+                    <div class="input-group mb-3">
+                      <textarea  class="form-control" rows="5" placeholder="Message" aria-label="Password" aria-describedby="password-addon" ></textarea>
+                    </div>
+                    <div class="text-center">
+                      <button type="button" class="btn bg-gradient-info btn-lg btn-rounded w-100 mt-4 mb-0">Contact US</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
