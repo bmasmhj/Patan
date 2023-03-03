0fbcf110d82a2f1146856ba324e14b2eac6663ce
bmasmhj <bmasmhj@gmail.com>
2023-03-03 09:24:37 +0545

    htacess - renaming and redireting

---
+++ error.php
@@ -0,0 +1,16 @@
+<?php require 'modal/header.php'; ?>
+<?php require 'modal/nav.php'; ?>
+<section class='mt-5 py-5'>
+    <div class="container mt-5">
+    </div>  
+</section>
+<section class=" mt-5 py-5">
+    <div class="container mt-5">
+        <div class="d-flex justify-content-center align-items-center">
+            <h1>Something went wrong</h1>
+        </div>
+    </div>  
+</section>
+
+<?php require 'modal/contact-us.php'; ?>
+<?php require 'modal/footer.php'; ?>
