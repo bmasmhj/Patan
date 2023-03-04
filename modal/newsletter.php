3c823260b43613d49c9280233dac3c5d0ce65043
bmasmhj <bmasmhj@gmail.com>
2023-03-04 16:06:50 +0545

    news letter

---
+++ modal/newsletter.php
@@ -0,0 +1,14 @@
+<div class="card">
+    <div class="card-body">
+        <h3>Subscribe</h3>
+        <p>Subscribe to our News Letter to notified abot latest News</p>
+        <div class="input-group mb-4">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+            <input class="form-control" placeholder="Your Email..." type="text">
+        </div>
+        <div class=" mx-auto">
+            <button type="button" class="btn bg-info w-100">Subscribe</button>
+        </div>
+        </div>
+    </div>
+</div>
