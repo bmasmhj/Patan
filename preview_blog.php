2a558b55c92b46ab04db4d8f990a101f03651c16
bmasmhj <bmasmhj@gmail.com>
2023-03-04 16:15:12 +0545

    preview blog

---
+++ preview_blog.php
@@ -0,0 +1,42 @@
+<?php require 'modal/header.php'; ?>
+<?php require 'modal/nav.php'; ?>
+<section class='mb-5 py-5'>
+    <div class="container mt-5">
+    </div>  
+</section>
+<section class='mt-5 py-5'>
+
+</section>
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <img src="" alt="jpg" >
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam pariatur architecto eaque ipsam ut voluptates deleniti fuga, ipsum dolor debitis, dignissimos veritatis quia illo! Reprehenderit optio necessitatibus minima nulla? Sint!</p>
+            <h1>News Heading 1</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis soluta mollitia iusto autem quae, accusamus facilis quod eveniet nulla assumenda unde molestias, voluptatibus magni porro magnam! Assumenda amet tempore est.</p>
+            <img src="" alt="jpg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum architecto consectetur asperiores culpa atque doloremque, id laudantium expedita et blanditiis exercitationem suscipit illum pariatur voluptatum tenetur necessitatibus? Dolore, reiciendis fuga?</p>
+            
+        </div>
+        <div class="col-4">
+            <div class="card p-2 mx-3">
+                <h4>Top News</h4>
+                <div class="card-body">
+                    <a href="" class="h6">  titlepopular post</a>
+                    <hr>
+                     <a href="" class="h6">  titlepopular post</a>
+                    <hr>
+                     <a href="" class="h6">  titlepopular post</a>
+                    <hr>
+                     <a href="" class="h6">  titlepopular post</a>
+                    <hr>
+                </div>
+            </div>
+            <?php require 'modal/newsletter.php' ?>
+        </div>
+    </div>
+</div>
+
+
+
+<?php require 'modal/footer.php'; ?>
