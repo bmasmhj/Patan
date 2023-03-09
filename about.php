af2abde20fc70af9906259fcf22711ed847466f0
bmasmhj <bmasmhj@gmail.com>
2023-03-09 09:30:34 +0545

    about us

--- about.php
+++ about.php
@@ -1,7 +1,24 @@
 <?php require 'modal/header.php'; ?>
 <?php require 'modal/nav.php'; ?>
 
-
+<header class="position-relative">
+  <div class="page-header min-vh-100" style="background-image: url(assets/img/aboutbg.jpg);">
+    <span class="mask bg-gradient-info"></span>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 text-start">
+          <h1 class="text-white" data-aos='fade-up'>About us</h1>
+          <p class="lead text-white text-start pe-5 mt-4" data-aos='fade-up' data-aos-delay='100'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium ea quam eos, consequatur, nemo ab eius deserunt rem totam quibusdam rerum, sit explicabo cumque error mollitia amet natus voluptatibus nostrum.</p>
+          <br />
+          <div class="buttons">
+            <button type="button" data-bs-toggle="modal" data-aos='fade-up' data-aos-delay='200' data-bs-target="#exampleModalSignup" class="btn btn-lg btn-white">Contact Us</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+</header>
 
 
 
