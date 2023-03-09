d9745aa57291e674d941cd9df8aea905194eb8e7
bmasmhj <bmasmhj@gmail.com>
2023-03-09 09:36:05 +0545

    href to onclick

--- modal/all_course.php
+++ modal/all_course.php
@@ -1,4 +1,5 @@
-
+<section class='mb-5 py-5'>
+</section>
 <section class="py-5 ">
   <div class="container">
     <div class="row">
@@ -10,18 +11,20 @@
 
     <?php 
     foreach ($allcoursedata as $key => $allcoursedataval)
-     echo ' <a href="javascript:void()" class="col-lg-4 col-md-6 mb-5 courses icon-move-right"  data-aos="fade-up" data-aos-delay="300">
+     echo ' <div onclick="window.location.href=\''.$base_url.'Detail/'.$allcoursedataval["code"].'\'" class="col-lg-4 col-md-6 mb-5 courses icon-move-right"  data-aos="fade-up" data-aos-delay="300">
         <div class="card course">
           <div class="card-body border-radius-lg position-relative overflow-hidden pb-4 px-5">
             <h5 class="mt-2">'.$allcoursedataval["name"].'</h5>
-            <p class="text-justify mb-3"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deleniti vitae repudiandae eum reiciendis quia ratione sed eius excepturi, expedita nesciunt qui iure voluptatem. Eligendi consectetur beatae velit eum itaque! </p>
+            <div class="course-desc text-justify mb-3"> 
+              '.Strip_tags(string_sub($allcoursedataval["description"] , 250)).'
+             </div>
             <div class="font-weight-bold text-xs text-uppercase font-weight-bolder text-info ">
                 See more
             <i class="fas fa-arrow-right text-xs ms-1"></i>
             </div>
           </div>
         </div>
-      </a>';
+      </div>';
       ?>
      
     </div>
