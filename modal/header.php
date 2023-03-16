f43836886fe987c14c63c7145280c45557e0c53c
NishesStha22 <sthanisi2020@gmail.com>
2023-03-16 14:07:11 +0545

    compromised

--- modal/header.php
+++ modal/header.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://bimash.com.np/Projects/Patan/assets/css/style.css?v=1.2.2">
     <link rel="stylesheet" href="https://bimash.com.np/Projects/Patan/assets/css/main.css?v=1.2.2">
-<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.0/css/all.css">
-<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.0/css/all.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <title>Patan Bca Guys</title>
 </head>
+
 <body>
-<?php require "controller/fetcher.php"?>
-    
+    <?php require "controller/fetcher.php" ?>
