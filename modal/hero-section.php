d46d27241ae01f74f72014c7616fe1ce82336e0b
bmasmhj <bmasmhj@gmail.com>
2023-03-21 14:18:38 +0545

    campus cheif detail

--- modal/hero-section.php
+++ modal/hero-section.php
@@ -11,6 +11,15 @@
             <button type="button" data-bs-toggle="modal" data-aos='fade-up' data-aos-delay='200' data-bs-target="#exampleModalSignup" class="btn btn-lg btn-white">Contact Us</button>
           </div>
         </div>
+        <div class="col-xl-2 offset-xl-2 col-md-3 offset-md-1">
+          <div class="card" data-aos='fade-up' >
+              <img src="campus-cheif.jpg" class='w-100' alt="">
+              <div class="card-body">
+                <h5 class="card-title">Campus Cheif</h5>
+                <p class="card-text">Dr. Laxman Singh Kunwar</p>
+              </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
